34656761648d92cd7c5deb6922ff129abdfcab98
chrisshortlaw <chris@chrisshortlaw.com>
2021-09-20 13:34:42 +0100

    Updated Models, added names & labels

--- models.py
+++ models.py
@@ -13,6 +13,10 @@ client = WOQLClient(endpoint)
 schema = WOQLSchema()
 
 class User(DocumentTemplate):
+    """
+    Class of user.
+    TODO: Add login, password(hashed), etc.
+    """
     _schema = schema
     _key = LexicalKey(['name'])
     name : str
@@ -21,9 +25,25 @@ class User(DocumentTemplate):
 
 
 class Game(DocumentTemplate):
+    """
+    Document for Computer and video games
+    fields with types:
+    name -> str
+    platform -> str
+    rank -> int
+    year -> int
+    genre -> str
+    publisher -> set(Publisher)
+    na_sales -> float
+    eu_sales -> float
+    jp_sales -> float
+    other_sales -> float
+    global_sales -> float
+    """
     _schema = schema
     _key = LexicalKey(['name', 'platform'])
     name: str
+    label: str
     platform: str
     rank: int
     year: int
@@ -39,9 +59,10 @@ class Game(DocumentTemplate):
 class Publisher(DocumentTemplate):
     _schema = schema
     _key = LexicalKey(['name'])
+    label: str
     name: str
 
 if __name__ == "__main__":
     client.connect(user=user, team=team, db="played_it_db", use_token=True) 
-    schema.commit(client, commit_msg="Add Game & Publisher DocTypes, added 'played_games'")
+    schema.commit(client, commit_msg="Changed publisher schema. Added 'key_name'. ")
 
