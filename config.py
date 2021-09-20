26bbd32692f4a92e607b0412dbf1221137d5411f
chrisshortlaw <chris@chrisshortlaw.com>
2021-09-20 13:42:55 +0100

    Added Config class to hold config variables

---
+++ config.py
@@ -0,0 +1,20 @@
+#!python3
+
+import os
+from terminusdb_client import WOQLClient
+
+class Config(object):
+
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'Latticed!Multitask!Sullen!Marital!Matchbox!Dedicate!Saucy!Overlying'
+
+    
+    TEAM = "team_of_me"
+
+    DATABASE_NAME = "played_it_db"
+
+    DB_URI = f"https://cloud.terminusdb.com/team_of_me/"
+
+# SAMPLE DB CONNECT CODE:
+# client=WOQLClient(Config.DB_URI)
+# client.connect(team=Config.TEAM, jwt_token=Config.TERMINUSDB_ACCESS_TOKEN, db=Config.DATABASE_NAME,)
+# Try setting token in environment variables
