eb835fac7416686638d99e8916edc5f0b25e30c9
chrisshortlaw <chris@chrisshortlaw.com>
2021-09-30 12:48:56 +0100

    Added User Registration

--- app.py
+++ app.py
@@ -7,9 +7,10 @@ if os.path.exists("env.py"):
 # from models import User
 from forms import LoginForm, RegisterForm
 from config import Config
-from models import played_it_db
+from models import played_it_db, User
 # from flask-login import LoginManager
 from flask import session
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 app = Flask(__name__)
@@ -28,6 +29,9 @@ played_it_db.db_connect()
 
 @app.route("/")
 def main():
+    """
+    grabs documents from db and displays on main page
+    """
     documents = played_it_db.client.get_document('Publisher/10tacle_studios')
     if 'username' in session:
         flash(f'Logged in as {session["username"]}')
@@ -41,7 +45,6 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user_list = list(played_it_db.client.query_document({"@type": "User", "name" : f"{form.username.data}"}))
-        print(user_list)
         if len(user_list) == 1:
             if user_list[0]['password'] == f"{form.password.data}":
                 session['username'] = user_list[0]['name']
@@ -58,8 +61,27 @@ def login():
 def register():
     form = RegisterForm()
     if form.validate_on_submit:
-        pass
+        user_email = form.email.data
+        existing_email_check = played_it_db.client.get_document(user_email)
+        if existing_email_check:
+            flash('User Account exists. Have you forgot your password?')
+            return render_template("register.html", title="Registration", form=form)
+        else:
+            user_name = form.username.data
 
+            user_pass = generate_password_hash(str(form.password.data), method='pbkdf2:sha256', salt_length=16)
+
+            new_user = User(email=user_email, name=user_name, _password=user_pass, played_games=[], reviews=[])
+
+            try:
+                played_it_db.client.insertDocument(new_user)
+            except Exception as e:
+                print(f'Error loading new User to database. Please try again. Error: {e}.')
+                return render_template("register.html", title="Registration", form=form)
+            else:
+                flash(f'New User: {user_name} has been successfully registered.')
+                session['username'] = user_name
+                return redirect(url_for('main'))
     return render_template("register.html", title="Registration", form=form)
 
 
