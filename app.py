e959ed64f08c3955be96bee9079599abb6aaf69a
chrisshortlaw <chris@chrisshortlaw.com>
2021-09-30 22:05:16 +0100

    Added login & registration functions

--- app.py
+++ app.py
@@ -11,20 +11,13 @@ from models import played_it_db, User
 # from flask-login import LoginManager
 from flask import session
 from werkzeug.security import generate_password_hash, check_password_hash
+from typing import Set
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
 played_it_db.db_connect()
 # login_manager = LoginManager()
-
-# user = 'chris'
-# team = 'team_of_me'
-# endpoint = f"https://cloud.terminusdb.com/{team}/"
-# client = WOQLClient(endpoint)
-
-
-# client.connect(user=user, team=team, db="new_test_db", use_token=True)
 
 
 @app.route("/")
@@ -42,11 +35,17 @@ def main():
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    """
+    TODO: Add server side validation (email etc.)
+    """
     form = LoginForm()
     if form.validate_on_submit():
-        user_list = list(played_it_db.client.query_document({"@type": "User", "name" : f"{form.username.data}"}))
+        user_id = str(form.username.data).replace('@','%40')
+        user_list = list(played_it_db.client.query_document({"@type": "User", "@id": f"User/{user_id}"}))
+        print(user_list)
+       # user_list = list(played_it_db.client.query_document({"@type": "User", "name" : f"{form.username.data}"}))
         if len(user_list) == 1:
-            if user_list[0]['password'] == f"{form.password.data}":
+            if check_password_hash(user_list[0]['_password'], form.password.data):
                 session['username'] = user_list[0]['name']
                 flash(f"{user_list[0]['name']} has successfully logged in!")
                 return redirect(url_for('main'))
@@ -54,34 +53,43 @@ def login():
                 flash('Incorrect Password')
         else:
             flash('Username Incorrect')
+    else:
+        print(form.errors)
     return render_template("login.html", title="Sign In", form=form)
 
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
+    """
+    TODO: Add server-side validation.
+    """
     form = RegisterForm()
-    if form.validate_on_submit:
-        user_email = form.email.data
-        existing_email_check = played_it_db.client.get_document(user_email)
-        if existing_email_check:
-            flash('User Account exists. Have you forgot your password?')
+    
+    if form.validate_on_submit():
+
+        user_email = played_it_db.client.query_document({"@type": "User", "email": f"{form.email.data}"})
+
+        if len(list(user_email)) > 1:
+            flash('User Account exists. Have you forgotten your password?')
             return render_template("register.html", title="Registration", form=form)
         else:
-            user_name = form.username.data
 
             user_pass = generate_password_hash(str(form.password.data), method='pbkdf2:sha256', salt_length=16)
 
-            new_user = User(email=user_email, name=user_name, _password=user_pass, played_games=[], reviews=[])
+            new_user = User(email=form.email.data, name=form.username.data, _password=user_pass, played_games=set(), reviews=set())
 
             try:
-                played_it_db.client.insertDocument(new_user)
+                played_it_db.client.insert_document(new_user)
             except Exception as e:
                 print(f'Error loading new User to database. Please try again. Error: {e}.')
                 return render_template("register.html", title="Registration", form=form)
             else:
-                flash(f'New User: {user_name} has been successfully registered.')
-                session['username'] = user_name
+                flash(f'New User: {form.username.data} has been successfully registered.')
+                session['username'] = form.username.data 
                 return redirect(url_for('main'))
+    else:
+        print('Registration form did not validate')
+        print(form.errors)
     return render_template("register.html", title="Registration", form=form)
 
 
