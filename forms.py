1d32f5a10d4019c74a5dcb5eb07b7a278805a8e0
chrisshortlaw <chris@chrisshortlaw.com>
2021-09-30 22:06:26 +0100

    Updt forms, fixed broken validation

--- forms.py
+++ forms.py
@@ -2,17 +2,25 @@
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, ValidationError, EqualTo
+from wtforms.validators import DataRequired, ValidationError, EqualTo, InputRequired, Email
 from wtforms.fields.html5 import EmailField
 from models import played_it_db
 
 
 class LoginForm(FlaskForm):
+    """
+    Login form for App.
+    Fields:
+        username -> StringField
+        password -> PasswordField
+        remember_me -> BooleanField
+        submit -> SubmitField
+    username & password have DataRequired() validator
+    """
     username = StringField('Username', validators=[DataRequired()], render_kw={"class": "input", "placeholder":"Username"})
     password = PasswordField('Password', validators=[DataRequired()], render_kw={"class": "input", "placeholder": "Password"})
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In', render_kw={"class": "button is-block is-info is-large is-fullwidth"})
-    
     
 
 class RegisterForm(FlaskForm):
@@ -26,22 +34,8 @@ class RegisterForm(FlaskForm):
             submit -> SubmitField
     """
     username = StringField('Username', validators=[DataRequired()], render_kw={"class": "input", "placeholder":"Enter a Username"})
-    password = PasswordField('Password', validators=[DataRequired()], render_kw={"class": "input", "placeholder": "Enter a Password"})
-    confirm = PasswordField('ConfirmPassword', validators=[DataRequired(), EqualTo('password')], render_kw={"class": "input", "placeholder": "Confirm Password"})
-    email = EmailField('Email', validators=[DataRequired()], render_kw={"class":"input", "placeholder": "Enter Your Email"})
+    password = PasswordField('Password', validators=[InputRequired(), DataRequired()], render_kw={"class": "input", "placeholder": "Enter a Password"})
+    confirm = PasswordField('ConfirmPassword', validators=[DataRequired(), EqualTo('password', message='Passwords must match')], render_kw={"class": "input", "placeholder": "Confirm Password"})
+    email = StringField('Email', validators=[DataRequired(), Email(message='Invalid Email Address')], render_kw={"class":"input", "placeholder": "Enter Your Email"})
     submit = SubmitField('Register', render_kw={"class": "button is-block is-info is-large is-fullwidth"})
 
-    # def validate_username(self, username):
-        # "Insert database query here for username"
-        # user = [Database Query]
-        # if user is not None:
-            # raise ValidationError('Username Taken. Please use a different username')
-
-    # def validate_email(self, email):
-   #      """
-        # Insert database query here.
-
-        # """
-        # # user = [Insert Database Query Here]
-        # if user is not None:
-            # raise ValidationError('Email Already Registered. Forgot your password? [Insert Link Here]')
