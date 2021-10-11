8201cfef5f9d512124f50121570b278b36340988
chrisshortlaw <chris@chrisshortlaw.com>
2021-10-11 20:21:36 +0100

    Added get_doc_obj function

--- database.py
+++ database.py
@@ -3,9 +3,9 @@
 import os
 from typing import TypeVar, Type
 
-from terminusdb_client import WOQLClient
+from terminusdb_client import WOQLClient, WOQLSchema 
 
-if os.path.exists('env'):
+if os.path.exists('env.py'):
     import env
 
 
@@ -28,7 +28,7 @@ class DBClient:
 
     def check_document_exists(self, dict: Type[dict]) -> bool:
         # TODO: Check if id is used  and write try...except to query by id
-        document = list(self.connect.query_document(dict))
+        document = list(self.client.query_document(dict))
         if len(document) == 1:
             return True
         else:
@@ -44,5 +44,52 @@ class DBClient:
         else:
             return None
 
+
+    def get_doc_obj(self, dict: Type[dict[str, str]]):
+        """
+        Params:
+            dict with either "@id" or "@type" or both.
+
+        Queries db and retrieves relevant document.
+        
+        Returns:
+            a WOQLSchema object of type corresponding to
+            that of queried document or None.
+        """
+
+        data_schema = WOQLSchema()
+        data_schema.from_db(self.client)
+
+        if "@id" in dict:
+            try:
+                raw_doc = self.client.get_document(dict["@id"])
+            except Exception:
+                print(Exception)
+                return None
+        elif "@type" in dict:
+            try:
+                raw_doc = list(self.client.query_document(dict))[0]
+            except Exception:
+                print(Exception)
+                return None
+        else:
+            raise TypeError("<dict> must have an '@id' or '@type' key.")
+            
+        link_type = data_schema.import_objects([raw_doc])[0]
+
+        return link_type
+            
+
 played_it_db = DBClient('chris', 'team_of_me', 'https://cloud.terminusdb.com/', 'new_test_db')
 
+
+def main():
+    played_it_db.db_connect()
+    print(played_it_db.get_doc_obj({"@id": "Publisher/nintendo"}))
+    print(played_it_db.get_doc_obj({"@type": "Publisher", "name": "alvion"}))
+
+
+
+
+if __name__ == "__main__":
+    main()
