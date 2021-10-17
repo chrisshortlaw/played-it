b7f68feedce1602bf5a7c7be96ce008857968bad
chrisshortlaw <chris@chrisshortlaw.com>
2021-10-17 15:32:04 +0100

    Removed default configuration settings.

--- new_config.py
+++ new_config.py
@@ -6,12 +6,8 @@ if os.path.exists('env.py'):
 
 class Config(object):
 
-    IP = os.environ.get("IP") or "0.0.0.0"
-
-    PORT = os.environ.get("PORT") or "5000"
-
-    SECRET_KEY = os.environ.get("SECRET_KEY") or 'Latticed!Multitask!Sullen!Marital!Matchbox!Dedicate!Saucy!Overlying'
-
-    MONGO_DBNAME = os.environ.get("MONGO_DBNAME") #or "played_it_db"
-
+    IP = os.environ.get("IP")
+    PORT = os.environ.get("PORT")
+    SECRET_KEY = os.environ.get("SECRET_KEY") 
+    MONGO_DBNAME = os.environ.get("MONGO_DBNAME")
     MONGO_URI = os.environ.get("MONGO_URI") 
